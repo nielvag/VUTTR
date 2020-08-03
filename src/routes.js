824aa03986309260c8930245e8b3667c5642e44f
nielvag <nielsonvagno@gmail.com>
2020-08-03 17:23:57 -0300

    Configuring eslint

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const {Router} = require('express');
+const { Router } = require('express');
 
 const routes = Router();
 
 routes.get('/', (req, res) => {
-  res.json({msg: 'It works!'});
-})
+  res.json({ msg: 'It works!' });
+});
 
 module.exports = routes;
